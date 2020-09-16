f0c38c9ac386a07f5381adbb38fe1c6f1ce78fc0
Abe101 <cyrusrohail101@gmail.com>
2020-09-16 18:49:16 +0500

    Built App

---
+++ build/precache-manifest.b3310b941272e112e4c7c8441a5a54d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b8e71d6de38d987bc901e09bfa12c9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ba2f2322cc62eb9f87c",
+    "url": "/static/css/main.009b573c.chunk.css"
+  },
+  {
+    "revision": "c09d2107add492cc6c96",
+    "url": "/static/js/2.466e0766.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.466e0766.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ba2f2322cc62eb9f87c",
+    "url": "/static/js/main.97e58192.chunk.js"
+  },
+  {
+    "revision": "9e19f21add552be81dd0",
+    "url": "/static/js/runtime-main.f5a95df1.js"
+  }
+]);
